08b1e6ce4ef054c6f712d54e3edb7e820a5f35fd
Randy Oest <oest@amazingrando.com>
2025-05-08 11:06:39 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -25,6 +25,11 @@ const nextConfig = {
         source: '/storybook/:path*',
         destination: '/storybook/:path*',
       },
+      {
+        source: '/',
+        destination: 'https://baruch-design.netlify.app/',
+        permanent: true,
+      },
     ];
   },
 };
